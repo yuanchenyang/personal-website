b2e78d8c9657e3b5c5433e2ffc58f33af30eb4bc
Chenyang Yuan <yuanchenyang@gmail.com>
2015-06-26 22:55:34 -0400

    Removed python visualizer

--- _configfiles/pull.sh
+++ _configfiles/pull.sh
@@ -1,6 +1,6 @@
 cd /var/www/particles && git pull -q
 cd /var/www/pyvisualizer/pyvisualizer && git pull -q
 cd /var/www/archiver/archiver && git pull -q
-cd /var/www/cs61a && git pull -q
 cd /var/www/coding-js && git pull -q
+cd /home/chenyang/caltime && git pull -q
 cd /var/www/web && git pull -q && /usr/local/bin/jekyll build > /dev/null
