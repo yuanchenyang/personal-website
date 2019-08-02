fd1b75f326aacd66c4ba915d053d23863eccc0b5
Chenyang Yuan <yuanchenyang@gmail.com>
2019-08-02 00:12:08 -0400

    Added dashboard

--- _configfiles/pull.sh
+++ _configfiles/pull.sh
@@ -2,6 +2,7 @@ cd /var/www/particles && git pull -q
 cd /var/www/pyvisualizer/pyvisualizer && git pull -q
 cd /var/www/archiver/archiver && git pull -q
 cd /var/www/coding-js && git pull -q
+cd /var/www/dashboard_app/dashboard && git pull -q
 cd /home/chenyang/caltime && git pull -q
 cd /var/www/web && git pull -q && /usr/local/bin/jekyll build > /dev/null
 cp ~/pdfs/* /var/www/web/_site
